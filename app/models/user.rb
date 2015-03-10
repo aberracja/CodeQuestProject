d23f46325ec735b6d8f440eedd88cd00cb96fdf0
aberracja <barbaraklosowska@gmail.com>
2015-03-10 15:10:52 +0100

    user sign up

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  validates :last_name, :first_name, presence: true
+end
